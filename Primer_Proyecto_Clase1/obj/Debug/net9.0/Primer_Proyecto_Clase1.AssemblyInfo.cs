1a679558799b13278fca72c08fb2d6be05d4118a
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-03-26 08:01:02 -0500

    branch

--- Primer_Proyecto_Clase1/obj/Debug/net9.0/Primer_Proyecto_Clase1.AssemblyInfo.cs
+++ Primer_Proyecto_Clase1/obj/Debug/net9.0/Primer_Proyecto_Clase1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Primer_Proyecto_Clase1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65a2e76ec37890e18c01d28cb8d84c0faeb7b243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f52275c8b7d9f7ddd5ad28bf48d762e140d5cd54")]
 [assembly: System.Reflection.AssemblyProductAttribute("Primer_Proyecto_Clase1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Primer_Proyecto_Clase1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
