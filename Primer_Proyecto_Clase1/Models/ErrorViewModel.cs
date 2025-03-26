d578866022e1aec7cac69e0270be92d757f6cc88
Mateo Cisneros <mateo.cisneros@udla.edu.ec>
2025-03-26 07:30:40 -0500

    Primer commit

---
+++ Primer_Proyecto_Clase1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Primer_Proyecto_Clase1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
